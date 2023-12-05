2764b2eb457f7a3a02319509be4d9dd7c6e4ee0c
gaurav6538 <152953607+gaurav6538@users.noreply.github.com>
2023-12-05 10:19:04 +0000

    1

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 122321
+let accountEmail = "gauravvilaniya@google.com"
+var accountPassword = "123145"
+accountCity = "Noida"
+
+// accountId = 2
+
+accountEmail = "trhgf@jhh.com"
+accountPassword = "21212132"
+accountCity = "Bombay"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope 
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; //
+
+ // alert( 3 + 3) //we are using nodejs, not browser 
+
+ console.log(3 + 3)
+
+ console.log("Gaurav")
+
+ //code readability should be high
+
+ let name = "Gaurav"
+ let age = 18
+ let isLoggedIn = false
+
+
+
+ // number => 2 to power 53
+ // bigint
+ // stringn => ""
+ // boolean => true/false
+ // null  => standalone value
+ // undefined => 
+ // symbol => unique
+
+
+
+
+
+
+ // object
+
+ console.log(typeof undefined); //undefined
+ console.log(typeof null); // object

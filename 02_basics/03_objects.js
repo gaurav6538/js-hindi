d55e01964958ea1e147574a3c3d9a8c43e1bf470
gaurav6538 <152953607+gaurav6538@users.noreply.github.com>
2023-12-10 09:33:01 +0000

    objest completed

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,16 @@
+// singleton
+
+
+// object literals
+const JsUser = {
+    name: "Gaurav"
+    "full name":
+    age: 19
+    location: "noida"
+    email: "Gaurav@google.com"
+    isLoggedIn: false,
+    lastlogInDays: ["Monday", "Saturday"]
+
+}
+
+
